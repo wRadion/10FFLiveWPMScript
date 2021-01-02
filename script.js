ee4def640d6c2b3d21a49eb6d93cdee5bf649deb
wRadion <wradion@gmail.com>
2021-01-02 16:22:39 +0100

    Bump to 4.0 - Remove jQuery dependencies

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         10FF Live WPM
 // @namespace    https://github.com/wRadion/10FFLiveWPMScript
-// @version      3.3
+// @version      4.0
 // @description  Live WPM for 10FF tests
 // @author       wRadion
 // @match        *://10fastfingers.com/typing-test/*
